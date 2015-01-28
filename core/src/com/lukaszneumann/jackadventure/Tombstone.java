7a22e0b667bf6e640034b139469091d81d417191
ShadowkhaN1 <lukneu000@utp.edu.pl>
2015-01-28 19:43:26 +0100

    Rejected commit: Default

--- core/src/com/lukaszneumann/jackadventure/Tombstone.java
+++ core/src/com/lukaszneumann/jackadventure/Tombstone.java
@@ -2,10 +2,7 @@ package com.lukaszneumann.jackadventure;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.*;
 
 /**
  * Created by Lukasz on 2015-01-15.
@@ -13,38 +10,116 @@ import com.badlogic.gdx.physics.box2d.PolygonShape;
 public class Tombstone extends Sprite {
 
     private MyGame myGame;
+    private WorldGame worldGame;
     private boolean isVisible = false;
+    private boolean isStationary = false;
     private Body body;
+    private Body bodyGround;
 
 
-    public Tombstone(MyGame myGame) {
+    public Tombstone(MyGame myGame, WorldGame worldGame) {
         this.myGame = myGame;
+        this.worldGame = worldGame;
 
-        set(new Sprite(myGame.content.getAssetManager().get("Health/Tombstone.png", Texture.class)));
-        setOriginCenter();
+        this.set(new Sprite(myGame.content.getAssetManager().get(myGame.assetsHelper.usesDpi + "/" + "Health/Tombstone.png", Texture.class)));
+        this.setPosition(myGame.WIDTH_SCREEN / 2 - this.getWidth() / 2, myGame.HEIGHT_SCREEN);
+        this.setOriginCenter();
+
+        createGround();
+
     }
 
 
-    private void createPhysics() {
+    public void update() {
+
+        if (isVisible == true) {
+
+            setX(body.getPosition().x * WorldGame.METERS_TO_PIXELS - this.getWidth() / 2);
+            setY(body.getPosition().y * WorldGame.METERS_TO_PIXELS - this.getHeight() / 2);
+
+
+            for (Contact contact : worldGame.getWorld().getContactList()) {
+
+                if (contact.getFixtureA().getBody() == body && contact.getFixtureB().getBody() == bodyGround ||
+                        contact.getFixtureA().getBody() == bodyGround && contact.getFixtureB().getBody() == body) {
+                    isStationary = true;
+                    body.setType(BodyDef.BodyType.StaticBody);
+
+                }
+            }
+
+        }
+
+
+    }
+
+
+    public boolean isStationary() {
+        return isStationary;
+    }
+
+    public void createPhysics(float x, float y) {
+
+        if (body == null) {
+
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(this.getWidth() / 2 * WorldGame.PIXELS_TO_METERS, this.getHeight() / 2 * WorldGame.PIXELS_TO_METERS);
+
+
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shape;
+            fixtureDef.density = 1f;
+            fixtureDef.restitution = 0f;
+            fixtureDef.friction = 0.01f;
+            fixtureDef.filter.categoryBits = myGame.filterCollision.getFilterCategory(FilterCollision.filterCategory.Tombstone);
+            fixtureDef.filter.maskBits = myGame.filterCollision.getFilterCategory(FilterCollision.filterCategory.Tombstone);
+
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.DynamicBody;
+            bodyDef.position.x = x * WorldGame.PIXELS_TO_METERS;
+            bodyDef.position.y = y * WorldGame.PIXELS_TO_METERS;
+
+            body = worldGame.getWorld().createBody(bodyDef);
+            body.createFixture(fixtureDef).setUserData("Tombstone");
+
+            body.setGravityScale(-100);
+
+            shape.dispose();
+
+        }
+    }
+
+    private void createGround() {
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(this.getWidth() / 2 * WorldGame.PIXELS_TO_METERS, this.getHeight() / 2 * WorldGame.PIXELS_TO_METERS);
+        shape.setAsBox(4 * this.getWidth() * WorldGame.PIXELS_TO_METERS, this.getHeight() / 35 * WorldGame.PIXELS_TO_METERS);
 
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
         fixtureDef.density = 1f;
-        fixtureDef.restitution = 0.4f;
-        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.5f;
         fixtureDef.filter.categoryBits = myGame.filterCollision.getFilterCategory(FilterCollision.filterCategory.Tombstone);
         fixtureDef.filter.maskBits = myGame.filterCollision.getFilterCategory(FilterCollision.filterCategory.Tombstone);
 
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.x =
+        BodyDef bodyDefGround = new BodyDef();
+        bodyDefGround.type = BodyDef.BodyType.StaticBody;
+        bodyDefGround.position.x = (myGame.WIDTH_SCREEN / 2 * WorldGame.PIXELS_TO_METERS);
+        bodyDefGround.position.y = 0;
 
+        bodyGround = worldGame.getWorld().createBody(bodyDefGround);
+        bodyGround.createFixture(fixtureDef).setUserData("groundBody");
 
+        shape.dispose();
 
     }
 
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean isVisible) {
+        this.isVisible = isVisible;
+    }
 }
